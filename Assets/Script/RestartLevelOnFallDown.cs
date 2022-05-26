0c596a11b670abde2e0bdaabd9443e2c3a203871
Marco Magra <marcomagra@archeido.com>
2022-05-26 23:08:01 +0200

    add player and try fall down reset

---
+++ Assets/Script/RestartLevelOnFallDown.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevelOnFallDown : MonoBehaviour
+{
+
+    [SerializeField] string strTag = "Player"; 
+    private void OnCollisionEnter(Collision collision){
+        
+        if(collision.collider.tag == strTag){   // if tag collide obj
+           SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Debug.Log(collision.collider.tag);
+            Debug.Log(strTag);
+        } 
+    }
+}
