e19c4d67f29ebbc458bda5b7d93370248c4f7904
Luca Baradel <baluca1996@gmail.com>
2022-05-28 16:30:21 +0200

    Companion Logic, UI and Instructions

--- Assets/TriggerUI.cs
+++ Assets/TriggerUI.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TriggerUI : MonoBehaviour
 {
     bool isOpen;
+    bool lockOpen;
     GameObject canvas;
     public GameObject companion;
     public float companionUISpeed = 5f;
@@ -12,11 +14,15 @@ public class TriggerUI : MonoBehaviour
     public float companionDistance = 5f;
     public float companionDistanceSide = 2f;
     public float companionDistanceTop = 1.5f;
+    string instructions;
+    TMP_Text tMPtext;
     // Start is called before the first frame update
     void Start()
     {
         canvas = GameObject.Find("InGameCanvas");
+        tMPtext = GameObject.Find("CompanionText").GetComponent<TMP_Text>();
         isOpen = false;
+        lockOpen = false;
     }
 
     // Update is called once per frame
@@ -24,17 +30,54 @@ public class TriggerUI : MonoBehaviour
     {
         if(Input.GetButtonDown("ToggleUI"))
         {
-            isOpen = isOpen ? false : true;
+            isOpen = (isOpen || lockOpen) ? false : true;
+            canvas.SetActive(isOpen || lockOpen);
         }
 
-        canvas.SetActive(isOpen);
-
-        if(isOpen) {
+        if(isOpen || lockOpen) {
             // Debug.Log(companion.transform.position);
             GameObject camera = GameObject.Find("Main Camera");
             Vector3 targetPosition = (camera.transform.position + camera.transform.forward * companionDistance + camera.transform.up * companionDistanceTop + camera.transform.right * companionDistanceSide);
             float distance = Mathf.Clamp(Vector3.Distance(companion.transform.position, targetPosition), 0.005f, 100f);
             companion.transform.position = Vector3.MoveTowards(companion.transform.position, targetPosition, companionUISpeed * Time.deltaTime * distance);
         }
+
+    }
+
+    public void showUI(string policy)
+    {
+        Debug.Log("LOCK UI");
+        lockOpen = true;
+        canvas.SetActive(true);
+        if(policy == "movement")
+        {
+            instructions = "Forse il viaggio ti ha stordito, ricordati che per muoverti devi premere i tasti W,A,S o D! Per guardarti attorno usa il mouse!";
+        }else if(policy == "orbInstructions")
+        {
+            instructions = "Questa sembra un fonte di energia sufficiente per un viaggio solo!";
+        }else {
+            instructions = "Error: Unhandled case!";
+        }
+
+        StartCoroutine("DisplayInstruction");
+    }
+
+    IEnumerator DisplayInstruction()
+    {
+        yield return new WaitForSeconds(1.5f); // After 1.5 sec
+        tMPtext.text = "";
+
+        foreach (char c in instructions)
+        {
+			tMPtext.text += c;
+			yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    public void unlockUI()
+    {
+        tMPtext.text = "";
+        lockOpen = false;
+        canvas.SetActive(false);
     }
 }
