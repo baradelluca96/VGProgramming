54390d33dcbc950110a3a28c45992e07fe69d840
Luca Baradel <baluca1996@gmail.com>
2022-05-29 16:40:54 +0200

    Teleport test

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -16,42 +16,54 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 velocity;
     bool isGrounded;
+    bool disabled;
 
     // Update is called once per frame
     void Update()
     {
+      if(!disabled){
+        // Checks if a small sphere around the GroundCheck object is colliding with something
+        // in this case a mask (a subgroup of objects or layer)
+        // Note: The player is not in this group, otherwis it will collide with himself and be always grunded;
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        if (isGrounded && velocity.y < 0)
+          {
+            velocity.y = -2f; // A constance force pushing on the ground;
+          }
 
-      // Checks if a small sphere around the GroundCheck object is colliding with something
-      // in this case a mask (a subgroup of objects or layer)
-      // Note: The player is not in this group, otherwis it will collide with himself and be always grunded;
-      isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-      
-      if (isGrounded && velocity.y < 0)
-        {
-          velocity.y = -2f; // A constance force pushing on the ground;
-        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-      float x = Input.GetAxis("Horizontal");
-      float z = Input.GetAxis("Vertical");
+        bool pressed = Input.GetKey("left shift");
+        float currentSpeed = baseSpeed;
+        if (pressed)
+          {
+            currentSpeed = currentSpeed * 2;
+          }
 
-      bool pressed = Input.GetKey("left shift");
-      float currentSpeed = baseSpeed;
-      if (pressed)
-        {
-          currentSpeed = currentSpeed * 2;
-        }
+        if(Input.GetButtonDown("Jump") && isGrounded)
+          {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+          }
 
-      if(Input.GetButtonDown("Jump") && isGrounded)
-        {
-          velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
+        Vector3 move = transform.right * x + transform.forward * z;
 
-      Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * currentSpeed * Time.deltaTime);
 
-      controller.Move(move * currentSpeed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime; // Current speed with acceleration, framerate independent;
 
-      velocity.y += gravity * Time.deltaTime; // Current speed with acceleration, framerate independent;
+        controller.Move(velocity * Time.deltaTime); // Real movement, should be framerate independent too, not connected to previous delta fix;
+      }
+    }
 
-      controller.Move(velocity * Time.deltaTime); // Real movement, should be framerate independent too, not connected to previous delta fix;
+    public bool Disable() {
+      disabled = true;
+      return disabled;
+    }
+
+    public bool Enable() {
+      disabled = false;
+      return disabled;
     }
 }
