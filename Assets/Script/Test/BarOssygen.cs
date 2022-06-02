41a7dbe7bba905f0caf07614476d52782ecc0f0e
Marco Magra <marcomagra@archeido.com>
2022-06-02 12:49:42 +0200

    try ossygen script

--- Assets/Script/Test/BarOssygen.cs
+++ Assets/Script/Test/BarOssygen.cs
@@ -7,23 +7,48 @@ public class BarOssygen : MonoBehaviour
 {
     public GameObject bar;
     public int time;
-    public GameObject progressText;
-    
+    public GameObject timeUpMessage;
+    public GameObject collider;
+    public bool activeBar = false;
+   
+
 
     void Start()
     {
-        AnimateBar();
+         if(activeBar == true){
+            AnimateBar();
+        }else{
+            LeanTween.scaleX(bar, 1, time);
+        }
     }
-
-
+    void Update() {
+        
+    }
+    
     public void AnimateBar() {
-        LeanTween.scaleX(bar, 0, time).setOnUpdate(setProgressText).setOnUpdateParam(progressText);
-        
-        
+        LeanTween.scaleX(bar, 0, time)
+        .setOnComplete(showMessage);    
+       // .setOnUpdate(setProgressText(time));
     }
 
-    public void setProgressText(float val, object obj) {
-        obj = val * 100f + "%";
+    void showMessage() {
+        timeUpMessage.SetActive(true);
+    }
+
+    /* public void setProgressText(float val) {
+        timeUpMessage.GetComponent<TextUI>().text = val * 100f + "%";
+      //  timeUpMessage.text = val * 100f + "%";
     } 
+    */
+
+    void OnTriggerEnter(Collider other) {
+        activeBar = true;
+        bar.SetActive(true);
+    }
+    
+    void OnTriggerExit(Collider other) {
+        activeBar = true;
+        bar.SetActive(false);
+    }
 
 }
