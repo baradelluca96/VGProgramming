661847bfa25c8f8fd568cde09abccec6aa7dc828
Marco Magra <marcomagra@archeido.com>
2022-06-09 22:37:17 +0200

    add resume game in menu

--- Assets/Script/MainMenu/MainMenu.cs
+++ Assets/Script/MainMenu/MainMenu.cs
@@ -8,6 +8,19 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public void PlayGame ()
     {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+    public void ReseumeGame()
+    {
         bool runComplete = PlayerPrefs.GetInt("Run") > 0;
         bool villageComplete = PlayerPrefs.GetInt("Village") > 0;
         bool pyramidComplete = PlayerPrefs.GetInt("Pyramid") > 0;
@@ -23,17 +36,5 @@ public class MainMenu : MonoBehaviour
         }
 
         SceneManager.LoadScene(scene);
-    }
-
-    // Update is called once per frame
-    public void QuitGame()
-    {
-        Debug.Log("Quit!");
-        Application.Quit();
-    }
-
-    public void ResetGame()
-    {
-        PlayerPrefs.DeleteAll();
     }
 }
