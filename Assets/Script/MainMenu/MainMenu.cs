bca7629d8b54aae4cb890122b680d162cadc5538
Marco Magra <marcomagra@archeido.com>
2022-06-10 17:45:06 +0200

    fix menu and robo anim

--- Assets/Script/MainMenu/MainMenu.cs
+++ Assets/Script/MainMenu/MainMenu.cs
@@ -7,7 +7,10 @@ public class MainMenu : MonoBehaviour
 {
     private void Start() {
         Cursor.visible = true;
+        Screen.lockCursor = false;
+       
     }
+
     // Start is called before the first frame update
     public void PlayGame ()
     {
